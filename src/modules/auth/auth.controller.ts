a649d36bfc92e7b2b26f23a9202b6f594db53da0
Sergey Murin <sergeimurin29@gmail.com>
2023-12-22 01:02:21 +0300

    registration token logic
    now registration endpoint returns user+token

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,24 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  InternalServerErrorException,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 import { IUser, User } from '../../schemas/user.schema';
-import { Observable } from 'rxjs';
+import {
+  catchError,
+  forkJoin,
+  from,
+  map,
+  mergeMap,
+  Observable,
+  of,
+  switchMap,
+} from 'rxjs';
 import { UsersService } from '../users/users.service';
 
 @Controller('auth')
@@ -19,7 +35,20 @@ export class AuthController {
   }
 
   @Post('registration')
-  registration(@Body() user: IUser): Observable<User> {
-    return this.usersService.createUser(user);
+  registration(
+    @Body() user: IUser,
+  ): Observable<User | { token: { access_token: string } }> {
+    //Creating a user
+    const createdUser$ = from(this.usersService.createUser(user)).pipe(
+      switchMap((userDocument) =>
+        of(this.usersService.obfuscateUser(userDocument)),
+      ),
+    );
+    //Creating a token
+    const token$ = createdUser$.pipe(
+      switchMap((user) => of(this.authService.login(user))),
+    );
+    
+    return forkJoin({ token: token$, user: createdUser$ });
   }
 }

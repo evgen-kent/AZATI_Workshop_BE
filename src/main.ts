b4758d79f9da13cca4d52e457b8ce67a3341a131
SergeyMurin <71402015+SergeyMurin@users.noreply.github.com>
2023-12-29 19:27:20 +0300

    Feature/env (#13)
    * remove .env file
    
    * add env.example
    
    * checking for env before app start

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ENV } from '../config/env.interface';
 import { ValidationPipe } from '@nestjs/common';
+import { loadEnvironmentVariables } from './utils/env.loader';
+import * as process from 'process';
 
 async function bootstrap() {
+  try {
+    loadEnvironmentVariables();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());

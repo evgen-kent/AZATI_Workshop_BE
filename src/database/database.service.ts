85a425a51a75f64cbda414e67983984d2fdc2b40
Sergey Murin <sergeimurin29@gmail.com>
2024-01-06 12:20:06 +0300

    init brands

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,52 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import * as path from 'path';
 import * as fs from 'fs';
+import { InjectModel } from '@nestjs/mongoose';
+import { Brand, BrandDocument } from './schemas/brands.schema';
+import { Model, Promise } from 'mongoose';
+
+type InitDocumentsType = BrandDocument;
 
 interface IDatabaseService {
   initializeAll(): Promise<void>;
 
-  initializeBrands(): Promise<void>;
-
-  initializeCategories(): Promise<void>;
-
-  initializeSizes(): Promise<void>;
-
-  initializeGenders(): Promise<void>;
-
-  initializeColors(): Promise<void>;
+  initialize(collection: Model<InitDocumentsType>, from: string): Promise<void>;
 }
 
 @Injectable()
 export class DatabaseService implements IDatabaseService {
-  constructor() {}
+  constructor(
+    @InjectModel(Brand.name) private readonly brandModel: Model<BrandDocument>,
+  ) {}
 
-  async initializeAll(): Promise<void> {}
+  async initializeAll(): Promise<void> {
+    await this.initialize(this.brandModel, 'brands.json');
+  }
 
-  async initializeBrands(): Promise<void> {}
-
-  async initializeCategories(): Promise<void> {}
-
-  async initializeColors(): Promise<void> {}
-
-  async initializeGenders(): Promise<void> {}
-
-  async initializeSizes(): Promise<void> {}
-
-  /*  async initializeUsers() {
-      try {
-        const usersExist = await this.userModel.find().exec();
-        if (usersExist.length === 0) {
-          const filePath = path
-            .join(__dirname, 'datasets', 'users.dataset.json')
-            .replace('\\dist', '');
-          const usersData = fs.readFileSync(filePath, 'utf8');
-          const users = JSON.parse(usersData);
-          await this.userModel.create(users);
-          console.log('Users initialized successfully.');
-        }
-      } catch (error) {
-        console.error('Error initializing users:', error);
+  async initialize(
+    collection: Model<InitDocumentsType>,
+    from: string,
+  ): Promise<void> {
+    try {
+      const collectionExists = await collection.find().exec();
+      if (collectionExists.length === 0) {
+        const filePath = path
+          .join(__dirname, 'datasets', from)
+          .replace('\\dist', '');
+        const dataString = fs.readFileSync(filePath, 'utf-8');
+        const data = JSON.parse(dataString);
+        await collection.create(data);
+        console.log(
+          `[Database Service] ${collection.modelName} initialized successfully.`,
+        );
       }
+    } catch (error) {
+      console.error(
+        `[Database Service] Error initializing ${collection.modelName}:`,
+      );
     }
-  */
+  }
 }

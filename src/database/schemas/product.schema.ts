32d130a339fc198581da27ede0339e2d8968f18a
Sergey Murin <sergeimurin29@gmail.com>
2024-01-11 11:47:48 +0300

    product schema

--- src/database/schemas/product.schema.ts
+++ src/database/schemas/product.schema.ts
@@ -1,47 +1,61 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, SchemaTypes } from 'mongoose';
 import { Optional } from '@nestjs/common';
 
 export interface IProduct {
-  productId: string;
-  name: string;
-  manufacturer: string;
-  width?: number;
-  height?: number;
-  depth?: number;
-  image?: string;
-  country_of_origin?: string;
+  title: string;
+  description: string;
+  cost: number;
+  discount: number | null;
+  total_cost: number;
+  rate: number;
+  category_id: string;
+  brands: string[];
+  colors: { id: string; available: boolean }[];
+  sizes: { id: string; available: boolean }[];
+  image: string;
+  additional_images: string[];
 }
 
 export type ProductDocument = Product & Document;
 
 @Schema()
-export class Product {
+export class Product implements IProduct {
   @Prop()
-  name: string;
+  title: string;
 
   @Prop()
-  manufacturer: string;
+  description: string;
 
   @Prop()
-  @Optional()
-  width?: number;
+  cost: number;
+
+  @Prop({ type: SchemaTypes.String, default: null, min: 1, max: 100 })
+  discount: number | null;
 
   @Prop()
-  @Optional()
-  height?: number;
+  total_cost: number;
+
+  @Prop({ min: 0, max: 5, default: 0 })
+  rate: number;
 
   @Prop()
-  @Optional()
-  depth?: number;
+  category_id: string;
 
   @Prop()
-  @Optional()
-  image?: string;
+  brands: string[];
 
   @Prop()
-  @Optional()
-  country_of_origin?: string;
+  colors: { id: string; available: boolean }[];
+
+  @Prop()
+  sizes: { id: string; available: boolean }[];
+
+  @Prop()
+  image: string;
+
+  @Prop()
+  additional_images: string[];
 }
 
 export const ProductSchema = SchemaFactory.createForClass(Product);

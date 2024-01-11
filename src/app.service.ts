58c0175067f39353a788c001e234f57d5b014ad0
Sergey Murin <sergeimurin29@gmail.com>
2024-01-11 14:01:23 +0300

    Ready to merge: (Tested) (Deploy branch changed on "main")

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getStatus(): string {
-    return 'Workshop BE app is ready! Now on deploy!';
+    return 'Workshop BE app is ready!';
   }
 }

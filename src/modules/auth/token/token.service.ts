25ef9dc6b3c790878c4deff85b73be19307c17ef
Sergey Murin <sergeimurin29@gmail.com>
2024-01-17 17:56:01 +0300

    token service

--- src/modules/auth/token/token.service.ts
+++ src/modules/auth/token/token.service.ts
@@ -1,9 +1,14 @@
-import { User } from '../../../database/schemas/user.schema';
-import { Injectable } from '@nestjs/common';
+import { UserDocument } from '../../../database/schemas/user.schema';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtPayload } from '../jwt/jwt.strategy';
+import { UserService } from '../../user/user.service';
+import { JwtAuthService } from '../jwt/jwt.auth.service';
+import { InjectModel } from '@nestjs/mongoose';
+import { Token, TokenDocument } from '../../../database/schemas/token.schema';
+import { Model } from 'mongoose';
 
 interface ITokenService {
-  generateTokensAsync(user: User): Promise<any>;
+  generateTokensAsync(user: UserDocument): Promise<any>;
 
   verifyRefreshTokenAsync(refreshToken: string): Promise<JwtPayload>;
 
@@ -12,11 +17,49 @@ interface ITokenService {
 
 @Injectable()
 export class TokenService implements ITokenService {
-  async generateTokensAsync(user: User): Promise<any> {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtAuthService: JwtAuthService,
+    @InjectModel(Token.name) private readonly tokenModel: Model<TokenDocument>,
+  ) {}
+
+  async generateTokensAsync(user: UserDocument): Promise<any> {
+    const accessToken =
+      await this.jwtAuthService.generateAccessTokenAsync(user);
+    const refreshToken =
+      await this.jwtAuthService.generateRefreshTokenAsync(user);
+
+    const tokenModel = new this.tokenModel({
+      user_id: user.id,
+      refresh_token: refreshToken,
+      created_at: Date.now(),
+    });
+
+    await tokenModel.save();
+
+    return {
+      access_token: accessToken,
+      refresh_token: refreshToken,
+    };
+  }
 
   async verifyRefreshTokenAsync(
     refreshToken: string,
-  ): Promise<JwtPayload | any> {}
+  ): Promise<JwtPayload | any> {
+    const decoded =
+      await this.jwtAuthService.verifyRefreshTokenAsync(refreshToken);
+    const storedToken = await this.tokenModel.findOne({
+      refresh_token: refreshToken,
+    });
 
-  async removeRefreshTokenAsync(refreshToken: string): Promise<void> {}
+    if (!storedToken || new Date() > storedToken.created_at) {
+      throw new UnauthorizedException('Invalid refresh token');
+    }
+
+    return decoded;
+  }
+
+  async removeRefreshTokenAsync(refreshToken: string): Promise<void> {
+    await this.tokenModel.findOneAndDelete({ refresh_token: refreshToken });
+  }
 }
